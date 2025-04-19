23b5a460a7a02cb235b7289d9015eca9b8332cb4
Neesa-Saud <nishasaud2061@gmail.com>
2025-04-19 19:06:27 +0545

    added

---
+++ lib/walpaper.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Walpaper extends StatefulWidget {
+  const Walpaper({super.key});
+
+  @override
+  State<Walpaper> createState() => _WalpaperState();
+}
+
+class _WalpaperState extends State<Walpaper> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
